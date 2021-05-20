9da7e02323c294d022cb7df6689a2f1a1bb654f3
mk3619s <malcolm.knoll@alumni.fh-aachen.de>
2021-05-20 17:28:01 +0200

    Login screen page layout update

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -6,11 +6,83 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+
+    void login() {
+    print('logged in!');
+  }
+
+  void pwfg() {
+    print('i forgot my password');
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      body: Text('login widget'),
-    ));
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 160),
+              child: Text(
+                'Luca QR Certification App',
+                style:
+                    TextStyle(fontStyle: FontStyle.italic, color: Colors.cyan, fontSize: 25),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter your username',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter your Password',
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 8),
+                
+                child: ElevatedButton(
+                  child: Text('Login'),
+                  onPressed: login,
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                      (Set<MaterialState> states) {
+                        if (states.contains(MaterialState.pressed))
+                          return Theme.of(context)
+                              .colorScheme
+                              .primary
+                              .withOpacity(0.5);
+                        return null; // Use the component's default.
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                Padding(
+                   padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                  child: TextButton(
+                    child: Text('Password Forgotten?'),
+                  onPressed: pwfg,
+                  style: TextButton.styleFrom(primary: Colors.purple),
+                ),
+              ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
